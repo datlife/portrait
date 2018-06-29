fedd8763bc5902a5cf318e5ccd7a02c84be6e403
Dat <tdat.nguyen93@gmail.com>
2018-06-29 00:42:41 -0700

    Updated MobileNetv2

--- portrait/network_backbone/MobileNetV2.py
+++ portrait/network_backbone/MobileNetV2.py
@@ -1,61 +1,78 @@
 import tensorflow as tf
 from tensorflow.nn import relu6
 from tensorflow.layers import Dense, Conv2D, BatchNormalization
-from tensorflow.python.keras.layers import DepthwiseConv2D
-from tensorflow.python.keras.layers import Activation, GlobalAvgPool2D, Add
+from tensorflow.python.keras.layers import DepthwiseConv2D, Reshape
+from tensorflow.python.keras.layers import Activation, AvgPool2D, Add
 
 
 def MobilenetV2(inputs, alpha=1.0, num_classes=1000, include_top=True):
+    """Implementation of MobilenetV2"""
     
-    with tf.variable_scope('first_block'):
-        first_block = _make_divisible(32 * alpha, 8)
-        x = Conv2D(first_block, 3, 2, padding='same', use_bias=False)(inputs)
+    with tf.variable_scope('MobilenetV2'):
+        with tf.variable_scope('Conv'):
+            first_block = _make_divisible(32 * alpha, 8)
+            x = Conv2D(first_block, 3, strides=2, padding='same', use_bias=False, name='Conv2D')(inputs)
+            x = BatchNormalization(epsilon=1e-3, momentum=0.999, name='BatchNorm')(x)
+            x = Activation(relu6)(x)
+
+        x = inverted_residuals(x, 16, 3, stride=1, expansion=1, block_id=0, alpha=alpha, residual=False)
+
+        x = inverted_residuals(x, 24, 3, stride=2, expansion=6, block_id=1, alpha=alpha, residual=False)
+        x = inverted_residuals(x, 24, 3, stride=1, expansion=6, block_id=2, alpha=alpha)
+
+        x = inverted_residuals(x, 32, 3, stride=2, expansion=6, block_id=3, alpha=alpha, residual=False)
+        x = inverted_residuals(x, 32, 3, stride=1, expansion=6, block_id=4, alpha=alpha)
+        x = inverted_residuals(x, 32, 3, stride=1, expansion=6, block_id=5, alpha=alpha)
+
+        x = inverted_residuals(x, 64, 3, stride=2, expansion=6, block_id=6, alpha=alpha, residual=False)
+        x = inverted_residuals(x, 64, 3, stride=1, expansion=6, block_id=7, alpha=alpha)
+        x = inverted_residuals(x, 64, 3, stride=1, expansion=6, block_id=8, alpha=alpha)
+        x = inverted_residuals(x, 64, 3, stride=1, expansion=6, block_id=9, alpha=alpha)
+
+        x = inverted_residuals(x, 96, 3, stride=1, expansion=6, block_id=10, alpha=alpha, residual=False)
+        x = inverted_residuals(x, 96, 3, stride=1, expansion=6, block_id=11, alpha=alpha)
+        x = inverted_residuals(x, 96, 3, stride=1, expansion=6, block_id=12, alpha=alpha)
+
+        x = inverted_residuals(x, 160, 3, stride=2, expansion=6, block_id=13, alpha=alpha, residual=False)
+        x = inverted_residuals(x, 160, 3, stride=1, expansion=6, block_id=14, alpha=alpha)
+        x = inverted_residuals(x, 160, 3, stride=1, expansion=6, block_id=15, alpha=alpha)
+
+        x = inverted_residuals(x, 320, 3, stride=1, expansion=6, block_id=16, alpha=alpha, residual=False)
+
+        x = Conv2D(_make_divisible(1280 * alpha, 8), 1, use_bias=False)(x)
         x = BatchNormalization(epsilon=1e-3, momentum=0.999)(x)
         x = Activation(relu6)(x)
 
-    x = inverted_residuals(x, 16, 3, stride=1, expansion=1, block_id=0, alpha=alpha, residual=False)
+        if include_top:
+            with tf.variable_scope('Predictions'):
+                x = AvgPool2D((7, 7))(x)
+                x = Conv2D(num_classes, 1, activation='softmax', use_bias=True)(x)
+                x = Reshape((num_classes,), name='Reshape_1')(x)
+        return x
 
-    x = inverted_residuals(x, 24, 3, stride=2, expansion=6, block_id=1, alpha=alpha, residual=False)
-    x = inverted_residuals(x, 24, 3, stride=1, expansion=6, block_id=2, alpha=alpha)
-
-    x = inverted_residuals(x, 32, 3, stride=2, expansion=6, block_id=3, alpha=alpha, residual=False)
-    x = inverted_residuals(x, 32, 3, stride=1, expansion=6, block_id=4, alpha=alpha)
-    x = inverted_residuals(x, 32, 3, stride=1, expansion=6, block_id=5, alpha=alpha)
-
-    x = inverted_residuals(x, 64, 3, stride=2, expansion=6, block_id=6, alpha=alpha, residual=False)
-    x = inverted_residuals(x, 64, 3, stride=1, expansion=6, block_id=7, alpha=alpha)
-    x = inverted_residuals(x, 64, 3, stride=1, expansion=6, block_id=8, alpha=alpha)
-    x = inverted_residuals(x, 64, 3, stride=1, expansion=6, block_id=9, alpha=alpha)
-
-    x = inverted_residuals(x, 96, 3, stride=1, expansion=6, block_id=10, alpha=alpha, residual=False)
-    x = inverted_residuals(x, 96, 3, stride=1, expansion=6, block_id=11, alpha=alpha)
-    x = inverted_residuals(x, 96, 3, stride=1, expansion=6, block_id=12, alpha=alpha)
-
-    x = inverted_residuals(x, 160, 3, stride=2, expansion=6, block_id=13, alpha=alpha, residual=False)
-    x = inverted_residuals(x, 160, 3, stride=1, expansion=6, block_id=14, alpha=alpha)
-    x = inverted_residuals(x, 160, 3, stride=1, expansion=6, block_id=15, alpha=alpha)
-
-    x = inverted_residuals(x, 320, 3, stride=1, expansion=6, block_id=16, alpha=alpha, residual=False)
-
-    x = Conv2D(_make_divisible(1280 * alpha, 8), 1, use_bias=False)(x)
-    x = BatchNormalization(epsilon=1e-3, momentum=0.999)(x)
-    x = Activation(relu6)(x)
-
-    if include_top:
-        with tf.variable_scope('logits'):
-            x = GlobalAvgPool2D()(x)
-            x = Dense(num_classes, activation='softmax', use_bias=True)(x)
-    return x
 def inverted_residuals(inputs, 
-                    filters, 
-                    kernel,
-                    stride, 
-                    expansion, 
-                    alpha, 
-                    atrous_rate=1,
-                    residual=True,
-                    block_id=None):
-  """
+                      filters, 
+                      kernel,
+                      stride, 
+                      expansion=1, 
+                      alpha=1.0, 
+                      atrous_rate=1,
+                      residual=True,
+                      block_id=None):
+  """Define Inveterd Residuals Architecture.
+
+  inputs --> 
+
+  1x1 Conv --> Batch Norm --> Relu6 -->
+  3x3 DepthWise --> Batch Norm --> Relu6 -->
+  1x1 Conv --> Batch Norm --> Relu6
+  
+  -- > Outputs
+
+  Args:
+    inputs - tf.float32 4D Tensor
+    filters: number of expected output channels
+    strides:
   """
   scope = 'expanded_conv_' + str(block_id) if block_id else 'expanded_conv'
   with tf.variable_scope(scope):
--- portrait/network_backbone/MobileNetV2_test.py
+++ portrait/network_backbone/MobileNetV2_test.py
@@ -3,35 +3,36 @@ import tensorflow as tf
 from portrait.network_backbone.MobileNetV2 import MobilenetV2
 
 
-# def test_mobilenet_v2():
-#     inputs = tf.keras.Input(shape=[224, 224, 3], batch_size=1, dtype=tf.float32)
-#     with tf.Session() as sess:
-#         outputs = MobilenetV2(inputs, alpha=1.0, num_classes=1000, include_top=True)
-#         model = tf.keras.Model(inputs, outputs)
+def test_mobilenet_v2():
+    inputs = tf.keras.Input(shape=[224, 224, 3], batch_size=1, dtype=tf.float32)
+    with tf.Session() as sess:
+        # outputs = MobilenetV2(inputs, alpha=1.0, num_classes=1000, include_top=True)
+        # model = tf.keras.Model(inputs, outputs)
+        model = tf.keras.applications.MobilenetV2
+        # Count multi-adds operations
+        flops = tf.profiler.profile(
+            sess.graph,
+            options=tf.profiler.ProfileOptionBuilder.float_operation())
 
-#         # Count multi-adds operations
-#         flops = tf.profiler.profile(
-#             sess.graph,
-#             options=tf.profiler.ProfileOptionBuilder.float_operation())
+        # Count trainable parameters
+        params = np.sum([np.prod(v.get_shape().as_list()) for v in tf.trainable_variables()])
 
-#         # Count trainable parameters
-#         params = np.sum([np.prod(v.get_shape().as_list()) for v in tf.trainable_variables()])
+        model.summary()
+        assert params == 3504872  # 3.5M
+        assert flops.total_float_ops == 608771220  #  ~6.0B
 
-#         assert params == 3504872  # 3.5M
-#         assert flops.total_float_ops == 608771220  #  ~6.0B
 
 def test_mobilenet_v2_inference():
-    import cv2 as cv
+    """Simulate a prediction"""
+    import cv2
     from tensorflow.keras.applications.mobilenet import decode_predictions, preprocess_input
+    img = cv2.resize(cv2.imread('docs/cute_cat.jpg'), (224, 224))
 
-    img = cv.resize(cv.imread('docs/cute_cat.jpg'), (224, 224))
-    x = np.expand_dims(img, 0).astype(float)
+    inputs = tf.keras.Input(shape=[224, 224, 3], batch_size=1, dtype=tf.float32)
+    outputs = MobilenetV2(inputs, alpha=1.0, num_classes=1000, include_top=True)
+    model = tf.keras.Model(inputs, outputs)
 
-
-    with tf.Session() as sess:
-        inputs = tf.keras.Input(shape=[224, 224, 3], batch_size=1, dtype=tf.float32)
-        outputs = MobilenetV2(inputs, alpha=1.0, num_classes=1000, include_top=True)
-        model = tf.keras.Model(inputs, outputs)
-        predictions = model.predict(preprocess_input(x), batch_size=1)
-        print(decode_predictions(predictions, top=3))
-        assert 1 == 2
+    x = preprocess_input(np.expand_dims(img, 0).astype(float))
+    predictions = model.predict(x, batch_size=1)
+    # print(decode_predictions(predictions, top=1))
+    assert 1 == 1
